3fe2a264410334db11b9031f71b757515cd6e25a
DESKTOP-F09MIE2\phucd <donkphuc@gmail.com>
2023-03-07 22:04:42 +0700

    hi

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = "Em";
+const giftUrl = "";
+const eventName = "Mở ra điiii";
+const titleCard = "Lê Vũ Anh Thư";
+const contentCard =
+  "Chúc em yêu của anh 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau!! Love you so much";
+const giftImage = "h2.png";
+const base64 = "hot-girl.png";
+const giftImageBase64 = "hot-girl.png" + base64;
